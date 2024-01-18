65ef6abc9f8988172124b95a80d61d94561bfbc7
mintzberg <majal7517@gmail.com>
2024-01-18 15:06:57 +0900

    2024-01-18 AM 레시피 삭제 메소드 추가 직관적이게 몇몇 메소드명 변경 관련 익셉션 추가. 추가된 익셉션은 나중에 권한없음을 사용할 다른 곳에도 쓸 수 있음.

--- src/main/java/com/tkf/teamkimfood/repository/query/RecipeQueryRepository.java
+++ src/main/java/com/tkf/teamkimfood/repository/query/RecipeQueryRepository.java
@@ -3,7 +3,9 @@ package com.tkf.teamkimfood.repository.query;
 import com.tkf.teamkimfood.domain.Recipe;
 import com.tkf.teamkimfood.domain.prefer.RecipeCategory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -19,8 +21,10 @@ public class RecipeQueryRepository {
         return em.createQuery(
                 "select r "+
                         "from Recipe r "+
-                        "where r.member = "+id
-        , Recipe.class).getResultList();
+                        "where r.member = :id "+
+                        "order by r.writeDate desc "
+        , Recipe.class).setParameter("id", id)
+                .getResultList();
     }
     //레시피 카테고리 받아온것 별로 조회(근데 3개가 정확히 일치되는것만 불러오는거일수도 있음)
     public List<Recipe> findAllWhereRecipeCategoryOrderByWriteDateDesc(RecipeCategory recipeCategory) {
@@ -39,5 +43,23 @@ public class RecipeQueryRepository {
                         "from Recipe r " +
                         "order by r.viewCount desc", Recipe.class
         ).getResultList();
+    }
+    //받아온 멤버아이디와 레시피아이디가 일치하는 레시피
+    public Recipe findOneWhereMemberIdAndRecipeId(Long memberId, Long recipeId) {
+        TypedQuery<Recipe> query = em.createQuery(
+                "select r " +
+                        "from Recipe r " +
+                        "where r.member.id = :memberId and r.id = :recipeId", Recipe.class
+        );
+
+        query.setParameter("memberId", memberId);
+        query.setParameter("recipeId", recipeId);
+
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            // 해당하는 결과가 없을 경우 예외 처리
+            return null;
+        }
     }
 }
