5a9cf1fe9172cf1504a9ad1bdd798c44c396368c
tae9e <qakl021@gmail.com>
2024-01-18 13:36:56 +0900

    jwt 토큰 설정

--- src/main/java/com/tkf/teamkimfood/repository/MemberRepository.java
+++ src/main/java/com/tkf/teamkimfood/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.tkf.teamkimfood.repository;
 import com.tkf.teamkimfood.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+import java.util.Optional;
 
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
