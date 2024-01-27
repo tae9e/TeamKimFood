902162618957c7fec36875deb4d3b6ee1970e8af
kdooit <kdooit@naver.com>
2024-01-28 04:49:50 +0900

    comment add

--- src/main/java/com/tkf/teamkimfood/domain/Comment.java
+++ src/main/java/com/tkf/teamkimfood/domain/Comment.java
@@ -2,19 +2,23 @@ package com.tkf.teamkimfood.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.BatchSize;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @Getter
+@Setter
 @Table(name = "comments")
 public class Comment {
 
@@ -22,7 +26,7 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(nullable = false)
     @Size(max = 300)
     private String content;
 
@@ -39,17 +43,31 @@ public class Comment {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "nickname")
+    private Member nickname;
+
     @BatchSize(size = 100)
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "recipe_id")
     private Recipe recipe;
 
+    @OneToMany
+    private List<Comment> comments = new ArrayList<>();
+
     //코멘트 데이터 넣을 때
     @Builder
-    public Comment(String content, LocalDateTime commentDate, LocalDateTime correctionDate) {
+    public Comment(Long id, String content, LocalDateTime commentDate, LocalDateTime correctionDate, Member member, Recipe recipe) {
+        this.id = id;
         this.content = content;
         this.commentDate = commentDate;
         this.correctionDate = correctionDate;
+        this.member = member;
+        this.recipe = recipe;
+    }
+
+    public void update(String content){
+        this.content = content;
     }
 
     //연관관계 메서드
@@ -61,5 +79,4 @@ public class Comment {
         this.recipe = recipe;
         recipe.getComments().add(this);
     }
-
 }
--- src/main/java/com/tkf/teamkimfood/dto/CommentDto.java
+++ src/main/java/com/tkf/teamkimfood/dto/CommentDto.java
@@ -1,27 +1,71 @@
 package com.tkf.teamkimfood.dto;
 
-import lombok.Data;
-import org.springframework.data.annotation.LastModifiedDate;
+import com.tkf.teamkimfood.domain.Comment;
+import com.tkf.teamkimfood.domain.Recipe;
+import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-@Data
 public class CommentDto {
 
-    private Long id;
-    private String content;
-    private LocalDateTime commentDate;
-    private LocalDateTime correctionDate;
-    private Long memberId;
-    private Long recipeId;
+//    private Long id;
+//    private String content;
+//    private LocalDateTime commentDate;
+//
+//    @LastcorrectionDate
+//    private LocalDateTime correctionDate;
+//
+//    private Long memberId;
+//    private String nickname;
+//    private Long recipeId;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public static class Request{
 
-    public CommentDto() {
+    private String content;
+
+    public Comment toEntity(Recipe recipe){
+        Comment comment = Comment.builder()
+                .recipe(recipe)
+                .content(content)
+                .build();
+
+        return comment;
     }
 
-    public CommentDto(Long id, String content, LocalDateTime commentDate, LocalDateTime correctionDate) {
-        this.id = id;
-        this.content = content;
-        this.commentDate = commentDate;
-        this.correctionDate = correctionDate;
+}
+
+    @RequiredArgsConstructor
+    @Getter
+    public static class Response{
+        private Long id;
+        private String content;
+        private String nickname;
+        private Long memberId;
+        private List<Response> comments;
+        private LocalDateTime commentDate;
+        private LocalDateTime correctionDate;
+        private Boolean isWriter;
+
+        public Response(Comment comment, Long currentMemberId, Long recipeWriterId){
+            this.id = comment.getId();
+            this.nickname = comment.getMember().getNickname();
+            this.memberId = comment.getMember().getId();
+            if(comment.getComments()!= null){
+                this.comments = comment.getComments().stream().map(c-> new CommentDto.Response(c, currentMemberId, recipeWriterId)).collect(Collectors.toList());
+            }
+            this.commentDate = comment.getCommentDate();
+            this.correctionDate = comment.getCorrectionDate();
+
+            if(Objects.equals(currentMemberId, comment.getMember().getId())) this.isWriter = true;
+            else this.isWriter = false;
+
+        }
     }
 }
