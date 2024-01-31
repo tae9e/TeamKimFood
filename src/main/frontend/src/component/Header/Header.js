3202d254739e53c85b75fe324295b2b83ae6495c
kdooit <kdooit@naver.com>
2024-01-31 15:32:35 +0900

    프론트 일부 수정

--- src/main/frontend/src/component/Header/Header.js
+++ src/main/frontend/src/component/Header/Header.js
@@ -1,19 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { DropdownSubmenu, NavDropdownMenu} from "react-bootstrap-submenu";
 import { SlLogin, SlPencil  } from "react-icons/sl";
 import '../Css/Common.css';
 
 function TopNav() {
 
-    return (
+    function toggleMenu() {
+        const parentMenu = document.querySelector('.parent-menu');
+
+        if (parentMenu.classList.contains('hover')) {
+            parentMenu.classList.remove('hover')
+        } else {
+            parentMenu.classList.add('hover')
+        }
+    }
+        return (
         <header>
-            <div className="hd_top">
+            <div className="hd-top">
                 <span className="logo"><a href="/">YoriJori</a></span>
                 <Form className="d-flex">
                     <Form.Control
@@ -24,54 +31,35 @@ function TopNav() {
                     />
                     <Button className="btn_regular" variant="outline-success">Search</Button>
                 </Form>
-                <ul className="signInUp">
-                    <li><a href="/signin"><SlPencil /> 회원가입</a></li>
-                    <li><a href="/login"><SlLogin /> 로그인</a></li>
-                </ul>
+                <div className="signInUp">
+                    <ul>
+                        <li><a href="/signin"><SlPencil /> 회원가입</a></li>
+                        <li><a href="/login"><SlLogin /> 로그인</a></li>
+                    </ul>
+                </div>
             </div>
 
-            <Navbar expand="lg" className="navbar-nav">
+            <Navbar expand="lg" className="navbar">
                 <Container fluid="true" id="contNav">
 
-                    {/*<Navbar.Toggle aria-controls="navbarScroll" />*/}
-                    {/*<Navbar.Collapse*/}
-                    {/*    id="navbarScroll"*/}
-                    {/*>*/}
-                    {/*    <Nav*/}
-                    {/*        className="navbar-text"*/}
-                    {/*        style={{maxHeight: '100px'}}*/}
-                    {/*        navbarScroll>*/}
-
-
-                            {/*<NavDropdown title="카테고리" id="navbarScrollingDropdown" className="menulist">*/}
-                            {/*    <NavDropdown.Item href="#action1">종류별*/}
-                            {/*        밑반찬 | 메인반찬 | 국/탕/찌개 | 디저트 | 면상황별/만두 | 밥/죽/떡 | 퓨전 | 김치/젓갈 | 장류/양념/소스/잼 | 양식 | 샐러드 | 스프 | 빵/과자 | 차/음료/술 | 기타</NavDropdown.Item>*/}
-                            {/*    <NavDropdown.Item href="#action2">상황별</NavDropdown.Item>*/}
-                            {/*    <NavDropdown.Item href="#action3">재료별</NavDropdown.Item>*/}
-                            {/*</NavDropdown>*/}
-                            {/*<Nav.Link href="#action4" className="menulist">오늘의 요리</Nav.Link>*/}
-                            {/*<Nav.Link href="#action5" className="menulist">인기 레시피</Nav.Link>*/}
-                            {/*<Nav.Link href="#action6" className="menulist">매거진</Nav.Link>*/}
-
-                            <ul className="depth-1 navbar-text" >
-                                <li className="category"><a href="#">카테고리</a></li>
+                            <ul className="navbar-nav" >
+                                <li className="depth-1 navbar-text parent-menu"><a href="#">카테고리</a></li>
                                     <ul>
-                                        <li className="nav-category-item">종류별 밑반찬 | 메인반찬 | 국/탕/찌개 | 디저트 | 면/만두 | 밥/죽/떡 | 퓨전
+                                        <li className="depth-2 nav-category-item">종류별 밑반찬 | 메인반찬 | 국/탕/찌개 | 디저트 | 면/만두 | 밥/죽/떡 | 퓨전
                                             | 김치/젓갈 | 장류/양념/소스/잼 | 양식 | 샐러드 | 스프 | 빵/과자 | 차/음료/술 | 기타
                                         </li>
-                                        <li className="nav-category-item">상황별</li>
-                                        <li className="nav-category-item">재료별</li>
+                                        <li className="depth-2 nav-category-item">상황별</li>
+                                        <li className="depth-2 nav-category-item">재료별</li>
                                     </ul>
-                                <li>오늘의 요리</li>
-                                <li>인기 레시피</li>
-                                <li>매거진</li>
+                                <li className="depth-1 navbar-text"><a href="#">오늘의 요리</a></li>
+                                <li className="depth-1 navbar-text"><a href="#">인기 레시피</a></li>
+                                <li className="depth-1 navbar-text"><a href="#">매거진</a></li>
                             </ul>
-                    {/*        </Nav.Link>*/}
 
-                    {/*    </Nav>*/}
-                    {/*</Navbar.Collapse>*/}
                 </Container>
             </Navbar>
+
+
 
         </header>
     );
